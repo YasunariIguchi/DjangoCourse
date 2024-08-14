a168350c83e58e61cc001bf942bf77a0a9b516de
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2024-08-14 16:48:58 +0900

    221

---
+++ CustomizedUser/accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserChangeForm, UserCreationForm
+# Register your models here.
+
+User = get_user_model()
+
+class CustomizeUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    
+    list_display = ("username","email","is_staff")
+    
+    fieldsets = (
+        ("ユーザー情報", {"fields": ("username", "email", "password", "picture")}),
+        ("パーミッション", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    
+    add_fieldsets = (
+        ("ユーザー情報", {"fields": ("username", "email", "password", "confirm_password")}),
+    )
+    
+admin.site.register(User, CustomizeUserAdmin)
